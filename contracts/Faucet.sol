2d820f2d3df527b97221f640a4b1e71367b1e0aa
minusta <mu.yang@gmail.com>
2021-12-06 14:39:16 +0200

    for solidity 0.8.10.

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: CC-BY-SA-4.0
-pragma solidity 0.6.4;
+pragma solidity 0.8.10;
 
 contract Faucet {
   receive() external payable {}
   
   function withdraw(uint withdraw_amount) public {
     require(withdraw_amount <= 100000000000000000);
-    msg.sender.transfer(withdraw_amount);
+    payable(msg.sender).transfer(withdraw_amount);
   }  
 }
