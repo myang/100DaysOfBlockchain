b36f2a679aece233d067cd4ffe1e8d65ead33950
minusta <myang@users.noreply.github.com>
2021-12-06 12:35:31 +0200

    Update Faucet.sol
    add license info and required compiler version.

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,8 +1,11 @@
+// SPDX-License-Identifier: CC-BY-SA-4.0
+pragma solidity 0.6.4;
+
 contract Faucet {
+  receive() external payable {}
+  
   function withdraw(uint withdraw_amount) public {
     require(withdraw_amount <= 100000000000000000);
     msg.sender.transfer(withdraw_amount);
-  }
-  
-  function () public payable {}
+  }  
 }
